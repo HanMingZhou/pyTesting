c653f45889f1d4ed645ddcc5d5d19595b2691fc1
hanmingzhou <1217155707@qq.com>
2024-12-07 14:44:20 +0800

    初始化项目

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+from selenium import webdriver
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+    driver = webdriver.Chrome()
+    driver.get("https://www.bing.com")
+
+    ## 阻塞主线程
+    driver.implicitly_wait(1000)
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
